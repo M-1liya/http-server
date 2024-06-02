e88692d18b92dd19af4463ad5fe4eafc4486a601
user <user@VMubuntu.myguest.virtualbox.org>
2024-06-02 16:32:04 +0700

    lab4

--- create_debpkg.sh
+++ create_debpkg.sh
@@ -7,9 +7,13 @@ cat << EOF > ./deb_pakage/DEBIAN/control
 Package: http-server
 Version: 1.0
 Architecture: amd64
+Depends: curl
 Maintainer: M-1liya
 Description: http server for DCSA labs
 EOF
+
+chmod 755 ./deb_pakage/DEBIAN/postinst
+chmod 755 ./deb_pakage/DEBIAN/prerm
 
 cp ./build/http-server ./deb_pakage/bin/
 
